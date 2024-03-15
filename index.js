0e2333d7d555651c9cee04a7c74b9490309cdbd3
VishnurajKarockal <vishnurajkarockal@gmail.com>
2024-03-16 00:13:16 +0530

    Kanban board assignment backend api

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 /* eslint-disable linebreak-style */
-const express = require("express")
-const { connection } = require("./config/db")
-const { UserRouter } = require("./routes/user.routes")
-const { TaskRouter } = require("./routes/task.routes")
-require("dotenv").config()
-const app = express()
+const express = require("express");
+const { connection } = require("./config/db");
+const { UserRouter } = require("./routes/user.routes");
+const { TaskRouter } = require("./routes/task.routes");
+require("dotenv").config();
+const cors = require("cors");
 
-app.use("/user", UserRouter)
-app.use("/task", TaskRouter)
+const app = express();
+app.use(cors());
+
+app.use("/", (req, res) => {
+  res.json({ msg: "Your Kanban Board API is working fine!" });
+});
+
+app.use("/user", UserRouter);
+app.use("/task", TaskRouter);
 app.listen(process.env.port, async () => {
-	await connection
-	console.log(`Server is running at port ${process.env.port}`)
-})
+  await connection;
+  console.log(`Server is running at port ${process.env.port}`);
+});
