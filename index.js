db31dfcf4c90c8ac277be03ddb88db5f4fd8479c
VishnurajKarockal <vishnurajkarockal@gmail.com>
2024-03-16 17:11:01 +0530

    Updated backend

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 /* eslint-disable linebreak-style */
-const express = require("express");
-const { connection } = require("./config/db");
-const { UserRouter } = require("./routes/user.routes");
-const { TaskRouter } = require("./routes/task.routes");
-require("dotenv").config();
-const cors = require("cors");
+const express = require("express")
+const { connection } = require("./config/db")
+const { UserRouter } = require("./routes/user.routes")
+const { TaskRouter } = require("./routes/task.routes")
+require("dotenv").config()
+const cors = require("cors")
 
-const app = express();
-app.use(cors());
+const app = express()
+app.use(cors())
 
-app.use("/user", UserRouter);
-app.use("/task", TaskRouter);
+app.use("/user", UserRouter)
+app.use("/task", TaskRouter)
 app.listen(process.env.port, async () => {
-  await connection;
-  console.log(`Server is running at port ${process.env.port}`);
-});
+	await connection
+	console.log(`Server is running at port ${process.env.port}`)
+})
